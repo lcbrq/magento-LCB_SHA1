e5c01ae4eff5106e508bf24f27430b59a1a5e018
Silpion <tom@leftcurlybracket.com>
2016-04-05 17:41:28 +0200

    Added readme, added possibility to set admin encryption to SHA-1 with constant change. Hotfixes customer password change from admin area.

---
+++ README.md
@@ -0,0 +1,3 @@
+# LCB_SHA1
+
+Replaces Magento encryption with SHA-1. Support for default admin panel MD-5 encryption.
--- app/code/community/LCB/SHA1/Model/Hash.php
+++ app/code/community/LCB/SHA1/Model/Hash.php
@@ -7,6 +7,8 @@
  */
 class LCB_SHA1_Model_Hash extends Mage_Core_Model_Encryption {
 
+    const FRONTEND_ONLY = true;
+
     /**
      * Override Mage_Core_Model_Encryption->hash()
      *
@@ -15,8 +17,19 @@ class LCB_SHA1_Model_Hash extends Mage_Core_Model_Encryption {
      */
     public function hash($data)
     {
+
+        if (!self::FRONTEND_ONLY) {
+            return hash('sha1', $data);
+        }
+
         if (!Mage::app()->getStore()->isAdmin()) {
             return hash('sha1', $data);
+        } else {
+            if (Mage::app()->getRequest()->getControllerName() == "customer" && Mage::app()->getRequest()->getActionName() == "save") {
+                if (debug_backtrace()[3]['function'] != "validateCurrentPassword") {
+                    return hash('sha1', $data);
+                }
+            }
         }
 
         return parent::hash($data);
